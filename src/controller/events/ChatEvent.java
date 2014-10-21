1e5e25ab45bcc7ad163b79ba4a1a13d4963486e6
ES359 <sphinxofpsl@gmail.com>
2014-10-20 21:28:44 -0400

    Updated Code.

--- src/controller/events/ChatEvent.java
+++ src/controller/events/ChatEvent.java
@@ -43,12 +43,12 @@ public class ChatEvent implements Listener {
              */
 
             String name = sm.getConfig().getString("custom-chat.Format");
-                name = name.replaceAll("%username%", player.getName());
-
-                  name = name.replaceAll("%message%", event.getMessage());
-
+                    name = name.replaceAll("%username%", player.getName());
+                    name = name.replaceAll("%message%", event.getMessage());
                     name = name.replaceAll("%world%", player.getWorld().getName());
-
+                    name = name.replaceAll("%UUID%", "" + player.getUniqueId());
+                    name = name.replaceAll("%IP%", "" +player.getAddress());
+                    
                    event.setFormat(ChatColor.translateAlternateColorCodes('&',name));
         }
 
